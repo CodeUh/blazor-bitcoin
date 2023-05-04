62ec52c740756b53d0a8e4c770be0d577ad6e030
CodeUh <CodeUh@gmail.com>
2023-05-04 18:23:28 -0500

    Initial Commit of work in progress.

---
+++ Server/Controllers/ApiController.cs
@@ -0,0 +1,63 @@
+﻿using BlazorBitcoin.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace BlazorBitcoin.Server.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class BlockchainController : ControllerBase
+    {
+        private readonly HttpClient _client;
+
+        public BlockchainController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("http://localhost:8332/");
+            var authValue = new AuthenticationHeaderValue(
+                "Basic", Convert.ToBase64String(
+                    System.Text.ASCIIEncoding.ASCII.GetBytes(
+                        $"{Environment.GetEnvironmentVariable("btcrpcuser")}:{Environment.GetEnvironmentVariable("btcrpcuser")}")));
+            _client.DefaultRequestHeaders.Authorization = authValue;
+        }
+
+        [HttpGet("blockchaininfo")]
+        public async Task<BlockchainInfo> GetBlockchainInfo()
+        {
+            //TODO:Hack to get around working api
+            var blockchainInfo = new BlockchainInfo() { Blocks=1 };
+            try
+            {
+                var request = new
+                {
+                    jsonrpc = "1.0",
+                    id = "1",
+                    method = "getblockchaininfo",
+                    @params = new object[] { }
+                };
+
+                var requestJson = JsonConvert.SerializeObject(request);
+                var content = new StringContent(requestJson, System.Text.Encoding.UTF8, "application/json");
+
+                var response = await _client.PostAsync("", content);
+                if (!response.IsSuccessStatusCode)
+                {
+                }
+
+                blockchainInfo = JsonConvert.DeserializeObject<BlockchainInfo>(await response.Content.ReadAsStringAsync());
+                return blockchainInfo;
+            }
+            catch(Exception ex)
+            {
+                //TODO:Hack to get around working api
+
+                return blockchainInfo;
+            }
+            
+        }
+    }
+}
---
+++ Shared/RpcResponse.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace BlazorBitcoin.Shared
+{
+    public class RpcResponse<T>
+    {
+        [JsonProperty("result")][JsonPropertyName("result")]public T Result { get; set; } = default(T);
+
+        [JsonProperty("error")][JsonPropertyName("error")]public RpcError Error { get; set; } = new RpcError();
+
+        [JsonProperty("id")][JsonPropertyName("id")]public string Id { get; set; } = string.Empty;
+    }
+
+    public class RpcError
+    {
+        [JsonProperty("code")][JsonPropertyName("code")]public int Code { get; set; } = 0;
+
+        [JsonProperty("message")][JsonPropertyName("message")]public string Message { get; set; } = string.Empty;
+
+        [JsonProperty("data")][JsonPropertyName("data")]public object Data { get; set; } = new object();
+    }
+}
